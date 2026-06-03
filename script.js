210eff8b412ad1f6e7a4a91ceffe60b68a790c42
Helium <130857426+Helium999@users.noreply.github.com>
2026-06-03 00:42:40 -0700

    Create a constructor for a book

---
+++ script.js
@@ -0,0 +1,8 @@
+let library = [];
+
+function book(name, author, genre, yearOfRelease) {
+    this.name = name;
+    this.author = author;
+    this.genre = genre;
+    this.yearOfRelease = yearOfRelease;
+}

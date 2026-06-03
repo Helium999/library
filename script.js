b57b658f1b392730008cce25dd0b083f8f5170bc
Helium <130857426+Helium999@users.noreply.github.com>
2026-06-03 00:55:23 -0700

    Add a random id to every book

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let library = [];
 
-function book(name, author, genre, yearOfRelease) {
+function Book(name, author, genre, yearOfRelease, id) {
     this.name = name;
     this.author = author;
     this.genre = genre;
     this.yearOfRelease = yearOfRelease;
+    this.id = id;
 }
 
 function addBookToLibrary(name, author, genre, yearOfRelease) {
-    newBook = new book(name, author, genre, yearOfRelease)
+    const id = crypto.randomUUID();
+    const newBook = new Book(name, author, genre, yearOfRelease, id);
     library.push(newBook);
 }

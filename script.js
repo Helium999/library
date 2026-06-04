0a1adcb71b92a9a2014b887564b9dddf55607ef6
Helium <130857426+Helium999@users.noreply.github.com>
2026-06-03 20:44:54 -0700

    Create functions to display each book separately on the page

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function addBookToLibrary(name, author, genre, yearOfRelease) {
     const newBook = new Book(name, author, genre, yearOfRelease, id);
     library.push(newBook);
 }
+
+function createBookCard(book) {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+
+    card.innerHTML = `
+        <h2>${book.name}</h2>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Genre:</strong> ${book.genre}</p>
+        <p><strong>Released:</strong> ${book.yearOfRelease}</p>
+        <p><strong>ID:</strong> ${book.id}</p>
+    `;
+
+    return card;
+}
+
+function displayBook(books) {
+    const container = document.querySelector("#library");
+
+    for(const book of books) {
+        card = createBookCard(book);
+        container.appendChild(card);
+    }
+}
